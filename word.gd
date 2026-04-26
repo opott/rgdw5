a959ea9938a893ade00e179df68f2f2cb9d15629
Oliver Potter <120502505+opott@users.noreply.github.com>
2026-04-26 16:33:09 +0100

    idek

--- word.gd
+++ word.gd
@@ -1,21 +1,28 @@
-extends Sprite2D
+extends Area2D
 
 var word: String
-var typed = ""
+var typed := ""
 
 func set_word(w: String):
 	word = w
+	typed = ""
 	$Label.text = word
-	
-func _input(event):
+
+func _unhandled_input(event):
 	if event is InputEventKey and event.pressed:
-		var char = char(event.unicode)
-		if char.is_valid_identifier():
-			typed += char
-			update_display()
-			if typed == word:
-				queue_free()
-				get_parent().get_node("Player").boost_jump()
-				
+		var c = char(event.unicode)
+
+		if c.length() == 1 and c >= "a" and c <= "z":
+			typed += c
+
+			if word.begins_with(typed):
+				update_display()
+
+				if typed == word:
+					queue_free()
+					get_parent().get_node("Player").boost_jump()
+			else:
+				typed = ""  # reset if wrong
+
 func update_display():
 	$Label.text = word.substr(typed.length())
